d72ae39347bf44e768dc0c69a9687a527f9faca3
GabrijelaJuricic <gabjuricic@gmail.com>
2022-06-09 18:30:29 +0200

    Uncomment no-orders placeholder

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -21,10 +21,10 @@ const MyOrders = () => {
           <h2>My Orders</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        {/* <div className="no-orders">
+        <div className="no-orders">
           <p>You have no orders.</p>
           <a href="">Create New Order</a>
-        </div> */}
+        </div>
       </div>
       <div className="my-order-right-side">
         <button className="my-orders-btn" onClick={pageHandler}>
